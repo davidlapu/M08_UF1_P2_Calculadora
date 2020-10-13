42dd1a1158d492fb67f7c80296b101f19c7ff51e
david <david.lama.7e3@itb.cat>
2020-10-13 19:01:19 +0200

    a

--- app/src/main/java/cat/itb/m08_uf1_p2_calculadora/MainActivity.java
+++ app/src/main/java/cat/itb/m08_uf1_p2_calculadora/MainActivity.java
@@ -3,12 +3,44 @@ package cat.itb.m08_uf1_p2_calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button button1, button2, button3, button4, button5, button6, button7, button8, button9,
+            button0;
+    private int num1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
+        button5 = findViewById(R.id.button5);
+        button6 = findViewById(R.id.button6);
+        button7 = findViewById(R.id.button7);
+        button8 = findViewById(R.id.button8);
+        button9 = findViewById(R.id.button9);
+        button0 = findViewById(R.id.button0);
+
+
+        button1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button b = (Button) v;
+        String bText = (String) b.getText();
+
+        switch (bText) {
+            case  "1" -> System.out.println(1);
+        }
+
+
     }
 }
